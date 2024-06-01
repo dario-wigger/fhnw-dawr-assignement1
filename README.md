6cc4afd7bc9c501d65040a49e4cb739a78059fbc
Dario Wigger <dario.wigger@students.fhnw.ch>
2024-06-01 14:36:36 +0200

    Assignement 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Allgemein
+Dieses Projekt beinhaltet vier Notebooks:
+* 1_data_loader.ipynb: Lädt die Daten
+* 2_data_preprocessor.ipynb: Validiert und korrigiert die Daten, vereinheitlicht die Struktur
+* 3_data_rating.ipynb: Erstellt die Ratings.
+* 4_data_visualization.ipynb: Visualiert Daten für den Bericht
+
+Die Resultate jedes Notebooks wird in einem eigenen Ordner abgelegt. Die Abgabe beinhaltet diese Files jeweils bereits, die Files werden aber überschrieben, wenn das Notebook ausgeführt wird.
+
+# Notwendige Pakete
+Im requirements.txt-File sind die notwendigen Pakete mit deren Version aufgeführt (erstellt mit pip freeze > requirements.txt).
+Grundsätzlich sollten alle Pakete standardmässig vorhanden sein ausser lxml, welches mit "pip install lxml" installiert werden kann.
---
+++ config.py
@@ -0,0 +1,24 @@
+SOURCES_DIR = "1_sources"
+PREPROCESSED_DIR = "2_preprocessed"
+RATED_DIR = "3_rated"
+VISUALS_DIR = "4_visuals"
+
+SOURCE_BFS_DATA_FOREIGNERS = SOURCES_DIR + "/bfs_data_foreigners.xlsx";
+PREPROCESSED_BFS_DATA_FOREIGNERS = PREPROCESSED_DIR + "/bfs_data_foreigners.csv";
+
+SOURCE_STADA_SOCIAL_ASSISTANCE_RATE = SOURCES_DIR + "/stada_data_social_assistance_rate.xlsx";
+PREPROCESSED_STADA_SOCIAL_ASSISTANCE_RATE = PREPROCESSED_DIR + "/stada_data_social_assistance_rate.csv";
+
+SOURCE_STADA_FISCAL_POWER = SOURCES_DIR + "/stada_data_fiscal_power.xlsx";
+PREPROCESSED_STADA_FISCAL_POWER = PREPROCESSED_DIR + "/stada_data_fiscal_power.csv";
+
+SOURCE_WIKIPEDIA_COMMUNITY_STATS = SOURCES_DIR + "/wikipedia_data_community_stats.csv";
+PREPROCESSED_WIKIPEDIA_COMMUNITY_STATS = PREPROCESSED_DIR + "/wikipedia_data_community_stats.csv";
+
+RATED_RATING_1 = RATED_DIR + "/rating_1.xlsx";
+RATED_RATING_2 = RATED_DIR + "/rating_2.xlsx";
+RATED_RATING_3 = RATED_DIR + "/rating_3.xlsx";
+
+VISUALS_COMMUNITY_AREAS = VISUALS_DIR + "/community_areas.png";
+VISUALS_SOCIAL_ASSISTANCE_RATE = VISUALS_DIR + "/social_assistance_rate";
+VISUALS_FOREIGNERS = VISUALS_DIR + "/foreigners.png";
